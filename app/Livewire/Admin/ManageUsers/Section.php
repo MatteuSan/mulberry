a74a27f949973a2fc148bf6d8ae98cf51fdd976c
Matteu <matteugt@gmail.com>
2024-04-15 03:31:41 +0800

    Implement keys and search

--- app/Livewire/Admin/ManageUsers/Section.php
+++ app/Livewire/Admin/ManageUsers/Section.php
@@ -2,23 +2,53 @@
 
 namespace App\Livewire\Admin\ManageUsers;
 
+use App\Models\Program;
+use App\Models\Role;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 use Livewire\Attributes\On;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
+use Livewire\WithoutUrlPagination;
+use Livewire\WithPagination;
 
 class Section extends Component
 {
-  #[On('user-data-updated')]
-  public function updateList(): void
+  use WithPagination, WithoutUrlPagination;
+
+  #[Validate('string')]
+  public string $searchQuery = '';
+
+  #[Validate('string')]
+  public string $roleFilter = 'all';
+
+  public Collection $roles;
+
+  public Collection $programs;
+
+  public function mount(): void
   {
-    $this->dispatch('$refresh');
+    $this->roles = Role::all();
+    $this->programs = Program::all();
+  }
+
+  #[On('user-list-updated')]
+  public function updateSection(): void
+  {
+    $this->resetPage();
   }
 
   public function render(): View
   {
+    $users = User::when($this->roleFilter !== 'all', fn (Builder $query) => $query->where('role_id', $this->roleFilter))
+      ->when($this->searchQuery !== '', fn (Builder $query) => $query->where(DB::raw("CONCAT(first_name, ' ', middle_name, ' ', last_name)"), 'like', "%$this->searchQuery%"))
+      ->paginate(15);
+
     return view('components.livewire.admin.manage-users.section', [
-      'users' => User::all()
+      'users' => $users,
     ]);
   }
 }
--- resources/views/components/livewire/admin/manage-users/section.blade.php
+++ resources/views/components/livewire/admin/manage-users/section.blade.php
@@ -1,20 +1,74 @@
-<div class="flex flow-column gap-md">
-  @if($users->count() >= 1)
-    @foreach($users as $user)
-      <livewire:admin.manage-users.card
-        :id="$user->id"
-        :prefix="$user->prefix"
-        :first-name="$user->first_name"
-        :middle-name="$user->middle_name"
-        :last-name="$user->last_name"
-        :suffix="$user->suffix"
-        :role="$user->role()->firstOrFail()->title"
-        :created-at="$user->created_at"
-      />
-    @endforeach
-  @else
-    <div class="grid pi-center border-xs border-surface-600 ink-surface-600">
-      <p>No users to load.</p>
+<section class="flex flow-column wrap-none gap-md">
+  <div class="flex flow-row wrap-none jc-space-between ai-center gap-xl">
+    <form class="w-full flex flow-row wrap-none jc-start ai-center gap-sm">
+      <label class="ms-form-field">
+        <span class="ms-form-field__label">Search a user...</span>
+        <input
+          type="text"
+          class="ms-form-field__input"
+          placeholder="Search a user..."
+          name="query"
+          value="{{ old('query') }}"
+          wire:model.live.debounce.500ms="searchQuery"
+          required
+        />
+      </label>
+      <label class="ms-select-field">
+        <span class="ms-select-field__label">Role</span>
+        <select name="role" class="ms-select-field__input" wire:model.live="roleFilter">
+          <option value="all">All</option>
+          @foreach($roles as $role)
+            <option value="{{ $role->id }}">{{ $role->title }}</option>
+          @endforeach
+        </select>
+      </label>
+    </form>
+    <button class="ms-button is-inverted is-filled" @click="toggle">
+      <i class="ms-button__icon">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+        </svg>
+      </i>
+      <span class="ms-button__label">Add</span>
+    </button>
+  </div>
+  <section
+    :class="`mu-modal__overlay${open ? ' is-open' : ''}`"
+    x-transition.opacity
+    x-show="open"
+    x-on:user-list-updated.window="open = false"
+  >
+    <div class="mu-modal" @click.outside="toggle">
+      <section class="w-full fill-surface-200 ink-surface-ink p-xl r-lg">
+        <div class="flex flow-row jc-center ai-center mt-md mb-2xl gap-md">
+          <h2 class="title" style="text-align: center">Add User</h2>
+        </div>
+        <livewire:admin.manage-users.register-form />
+      </section>
     </div>
-  @endif
-</div>
+  </section>
+  <div class="flex flow-column gap-md">
+    @if($users->count() > 0)
+      @foreach($users as $user)
+        <livewire:admin.manage-users.card
+          :id="$user->id"
+          :prefix="$user->prefix"
+          :first-name="$user->first_name"
+          :middle-name="$user->middle_name"
+          :last-name="$user->last_name"
+          :suffix="$user->suffix"
+          :role="$user->role()->firstOrFail()->title"
+          :created-at="$user->created_at"
+          :wire:key="$user->id"
+        />
+      @endforeach
+    @else
+      <div class="grid pi-center border-xs border-surface-600 ink-surface-600">
+        <p>No users to load.</p>
+      </div>
+    @endif
+  </div>
+  <div class="flex flow-row jc-center ai-center gap-md">
+    {{ $users->links('components.livewire.components.ms-pagination') }}
+  </div>
+</section>
