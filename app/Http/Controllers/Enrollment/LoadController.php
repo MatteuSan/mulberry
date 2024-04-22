d4d864e6baa40a4704eab6f726a387d6e90d6462
Matteu <matteugt@gmail.com>
2024-04-23 03:55:58 +0800

    Transfer values

--- app/Http/Controllers/Enrollment/LoadController.php
+++ app/Http/Controllers/Enrollment/LoadController.php
@@ -3,14 +3,11 @@
 namespace App\Http\Controllers\Enrollment;
 
 use App\Http\Controllers\Controller;
-use App\Models\Course;
 
 class LoadController extends Controller
 {
   public function render()
   {
-    return view('pages.main.enrollment.load', [
-      'courses' => Course::all(),
-    ]);
+    return view('pages.main.enrollment.load');
   }
 }
