1badfe031775669af043f1d426511f7e11e1af5e
Matteu <matteugt@gmail.com>
2024-04-22 23:15:24 +0800

    Delete old views

--- resources/views/pages/main/enrollment.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.main')
-
-@section('title', 'Enrollment')
-
-@section('content')
-  <main class="content-wrap">
-    <h2 class="title">Enrollment Page</h2>
-  </main>
-@endsection
--- resources/views/pages/main/profile.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.main')
-
-@section('title', 'Profile')
-
-@section('content')
-  <main class="content-wrap">
-    <h2 class="title">Profile Page</h2>
-  </main>
-@endsection
