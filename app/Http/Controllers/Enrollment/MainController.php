d638b21642cf3ffcade76ba6730d805b8b82feb8
Matteu <matteugt@gmail.com>
2024-04-24 14:03:48 +0800

    Update enrollment

--- app/Http/Controllers/Enrollment/MainController.php
+++ app/Http/Controllers/Enrollment/MainController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers\Enrollment;
 
 use App\Http\Controllers\Controller;
+use App\Models\Course;
+use App\Models\LoadRequest;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
   public function render()
   {
-    return view('pages.main.enrollment.index');
+    return view('pages.main.enrollment.index', [
+      'load' => Course::orderBy('name')->get()->filter(fn (Course $course) => auth()->user()->loadedCourses()->where('course_id', $course->id)->exists()),
+      'isRequestApproved' => LoadRequest::where('student_id', auth()->id())?->first()?->is_approved,
+    ]);
   }
 }
