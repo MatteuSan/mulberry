0f6b1c04864319ab22068666277349b43de9e325
Matteu <matteugt@gmail.com>
2025-03-11 12:37:04 +0800

    Add the update for the project thingamajig

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,13 +5,27 @@
   <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>@yield('title') - Mulberry</title>
+  <title>@yield('title') - SCCS Dashboard</title>
   <link rel="stylesheet" href="{{ Vite::asset('resources/scss/main.scss') }}">
   <script type="text/javascript" src="{{ Vite::asset('resources/js/app.js') }}" defer></script>
 
-  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/img/apple-touch-icon.png') }}">
-  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/img/32x32-icon.png') }}">
-  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/img/16x16-icon.png') }}">
+  <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('icons/apple-icon-57x57.png') }}">
+  <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('icons/apple-icon-60x60.png') }}">
+  <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('icons/apple-icon-72x72.png') }}">
+  <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('icons/apple-icon-76x76.png') }}">
+  <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('icons/apple-icon-114x114.png') }}">
+  <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('icons/apple-icon-120x120.png') }}">
+  <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('icons/apple-icon-144x144.png') }}">
+  <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('icons/apple-icon-152x152.png') }}">
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('icons/apple-icon-180x180.png') }}">
+  <link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('icons/android-icon-192x192.png') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('icons/favicon-32x32.png') }}">
+  <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('icons/favicon-96x96.png') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('icons/favicon-16x16.png') }}">
+  <link rel="manifest" href="{{ asset('manifest.json') }}">
+  <meta name="msapplication-TileColor" content="#7d70d2">
+  <meta name="msapplication-TileImage" content="{{ asset('icons/ms-icon-144x144.png') }}">
+  <meta name="theme-color" content="#7d70d2">
 
   @livewireStyles
 </head>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>@yield('title') - Mulberry</title>
+  <title>@yield('title') - SCCS Dashboard</title>
   @vite('resources/scss/main.scss')
   @vite('resources/js/app.js')
 
@@ -13,9 +13,23 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
 
-  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/img/apple-touch-icon.png') }}">
-  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/img/32x32-icon.png') }}">
-  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/img/16x16-icon.png') }}">
+  <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('icons/apple-icon-57x57.png') }}">
+  <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('icons/apple-icon-60x60.png') }}">
+  <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('icons/apple-icon-72x72.png') }}">
+  <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('icons/apple-icon-76x76.png') }}">
+  <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('icons/apple-icon-114x114.png') }}">
+  <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('icons/apple-icon-120x120.png') }}">
+  <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('icons/apple-icon-144x144.png') }}">
+  <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('icons/apple-icon-152x152.png') }}">
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('icons/apple-icon-180x180.png') }}">
+  <link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('icons/android-icon-192x192.png') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('icons/favicon-32x32.png') }}">
+  <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('icons/favicon-96x96.png') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('icons/favicon-16x16.png') }}">
+  <link rel="manifest" href="{{ asset('manifest.json') }}">
+  <meta name="msapplication-TileColor" content="#7d70d2">
+  <meta name="msapplication-TileImage" content="{{ asset('icons/ms-icon-144x144.png') }}">
+  <meta name="theme-color" content="#7d70d2">
 
   <meta name="robots" content="noindex, nofollow">
 
@@ -25,7 +39,7 @@
 <header class="mu-header">
   <div class="mu-header__wrapper">
     <a wire:navigate href="{{ route('home') }}">
-      <img src="{{ asset('/img/mapua-logo.png') }}" alt="logo" class="mu-logo">
+      <img src="{{ asset('/img/sscs-logo.png') }}" alt="logo" class="mu-logo">
     </a>
     <nav class="mu-appbar">
       <ul class="mu-appbar__list{{ auth()->user()->hasRole('superuser') ? ' is-superuser' : '' }}">
