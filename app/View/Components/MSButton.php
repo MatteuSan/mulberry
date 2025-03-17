789a70aa239e0519e192a5efc0c64efac27fcc81
Matteu <matteugt@gmail.com>
2025-03-18 02:16:24 +0800

    Change how functions work in components

--- app/View/Components/MSButton.php
+++ app/View/Components/MSButton.php
@@ -16,8 +16,33 @@ class MSButton extends Component
     public bool $isDisabled = false
   ) {}
 
+  function handleTypes(): string
+  {
+    $finalTypes = [];
+    $types = explode(' ', $this->type);
+    foreach ($types as $type) $finalTypes[] = "is-$type";
+    return implode(" ", $finalTypes);
+  }
+
+  private function handleLinkTarget(string $fallbackTarget = '_self'): string
+  {
+    if (Route::getRoutes()->hasNamedRoute($this->link)) return route($this->link);
+    $isExternal = (str_contains($this->link, 'http://') || str_contains($this->link, 'https://')) && !Route::getRoutes()->hasNamedRoute($this->link);
+    return $isExternal ? '_blank' : $fallbackTarget;
+  }
+
+  private function handleLink(): string
+  {
+    if (Route::getRoutes()->hasNamedRoute($this->link)) return route($this->link);
+    return $this->link;
+  }
+
   public function render(): View|Closure|string
   {
-    return view('components.ms-button');
+    return view('components.ms-button', [
+      'linkTarget' => $this->handleLinkTarget(),
+      'link' => $this->handleLink(),
+      'type' => $this->handleTypes(),
+    ]);
   }
 }
--- resources/views/components/ms-button.blade.php
+++ resources/views/components/ms-button.blade.php
@@ -5,38 +5,16 @@
   'nativeType' => 'button',
   'isDisabled' => false,
   'link' => '',
+  'linkTarget' => '_self',
 ])
 
-@php
-  function handleLinkTarget(string $link, string $fallbackTarget = '_self'): string
-  {
-   if (Route::getRoutes()->hasNamedRoute($link)) return route($link);
-   $isExternal = (str_contains($link, 'http://') || str_contains($link, 'https://')) && !Route::getRoutes()->hasNamedRoute($link);
-   return $isExternal ? '_blank' : $fallbackTarget;
-  }
-
-  function handleLink(string $link): string
-  {
-   if(Route::getRoutes()->hasNamedRoute($link)) return route($link);
-   return $link;
-  }
-
-  function handleTypes(string $types): string
-  {
-   $finalTypes = [];
-   $types = explode(' ', $types);
-   foreach ($types as $type) $finalTypes[] = "is-$type";
-   return implode(" ", $finalTypes);
-  }
-@endphp
-
 @if(!empty($link))
-  <a class="ms-button{{ $type ? ' ' . handleTypes($type) : '' }}" href="{{ handleLink($link) }}" target="{{ handleLinkTarget($link) }}" {{ $attributes }} wire:navigate @if($isDisabled) disabled @endif>
+  <a class="ms-button{{ $type ? ' ' . $type : '' }}" href="{{ $link }}" target="{{ $linkTarget }}" {{ $attributes }} role="{{ $role }}" type="{{ $nativeType }}" @if($isDisabled) disabled @endif>
     @if($icon) <i class="ms-button__icon" aria-hidden="true">{{ $icon }}</i> @endif
     <span class="ms-button__label">{{ $slot }}</span>
   </a>
 @else
-  <button class="ms-button{{ $type ? ' ' . handleTypes($type) : '' }}" {{ $attributes }} role="{{ $role }}" type="{{ $nativeType }}" @if($isDisabled) disabled @endif>
+  <button class="ms-button{{ $type ? ' ' . $type : '' }}" {{ $attributes }} role="{{ $role }}" type="{{ $nativeType }}" @if($isDisabled) disabled @endif>
     @if($icon) <i class="ms-button__icon" aria-hidden="true">{{ $icon }}</i> @endif
     <span class="ms-button__label">{{ $slot }}</span>
   </button>
