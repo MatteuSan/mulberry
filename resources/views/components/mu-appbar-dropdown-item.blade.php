e5b4d3876eb631f5fec90c0c94d7e896bb7c5965
Matteu <matteugt@gmail.com>
2025-03-18 00:50:07 +0800

    Update appbar

--- resources/views/components/mu-appbar-dropdown-item.blade.php
+++ resources/views/components/mu-appbar-dropdown-item.blade.php
@@ -1,3 +1,9 @@
-<li class="ms-list__item{{ $rn($route) ? ' is-active' : '' }}">
+@props(['route'])
+
+@php
+  $isActive = Route::currentRouteName() == $route;
+@endphp
+
+<li class="ms-list__item{{ $isActive ? ' is-active' : '' }}">
   <a wire:navigate href="{{ route($route) }}">{{ $slot }}</a>
 </li>
